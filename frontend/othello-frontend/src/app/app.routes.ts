692212e96c4db62f45ebbbb3f1fc7348e763e93c
DChartrain-3dArtist <donovan.chartrain@gmail.com>
2025-09-02 11:29:18 +0200

    preparing deploy branch

--- frontend/othello-frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { GameComponent } from './game/game.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'game', component: GameComponent },
-];
